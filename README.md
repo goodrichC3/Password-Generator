95853a7753e1455e95ded312d8b5050d4fe4d1d1
CyberCrodic <chris.goodrich90@gmail.com>
2023-01-25 13:34:13 -0500

    first draft of JS code. Updated README

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Password-Generator
+
+
+## Description
+JavaScript program to create a randomized password with varying criteria. User should be able to choose password length between 8 and 128 characters, and have different combinations of: lower case letters, upper case letters, numbers, and special characters.
+
+## Lisence
+MIT
--- script.js
+++ script.js
@@ -1,14 +1,58 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+ // Assignment Code
+ var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+ // Write password to the #password input
+ function writePassword() {
+   var password = generatePassword();
+   var passwordText = document.querySelector("#password");
+ 
+   passwordText.value = password;
+ 
+ }
+ 
+ // Add event listener to generate button
+ generateBtn.addEventListener("click", writePassword);
+ 
+ 
+ 
+ 
+ // Var outside functions for scope
+ var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+ var upperCase = lowerCase.toUpperCase();
+ var randomNumber = "0123456789";
+ var symbol = "!@#$%^&*()";
+ var charsArr = [lowerCase, upperCase, randomNumber, symbol];
+ 
+ function getRandomLower() {
+   return lowerCase[Math.floor(Math.random() * lowerCase.length)];
+ }
+ 
+ function getRandomUpper() {
+   return upperCase[Math.floor(Math.random() * upperCase.length)];
+ }
+ 
+ function getRandomNumber() {
+   return randomNumber[Math.floor(Math.random() * randomNumber.length)];
+ }
+ 
+ function getRandomSymbol() {
+   return symbol[Math.floor(Math.random() * symbol.length)];
+ }
+ 
+ // All additional code should be attainable in this function. PLay with helper functions.
+ // Try prompt() and confirm()
+ 
+ 
+ function generatePassword() {
+   var lengthChoice = window.prompt("Choose password length:");
+   var lengthChoice = parseInt(lengthChoice);
+   if (!lengthChoice) {
+     return;
+   } else if (lengthChoice.length < 8 || lengthChoice.length > 128) {
+     window.confirm("Password must be between 8 and 128 characters in length.");
+   } else if (lengthChoice,length >= 8 || lengthChoice.length <= 128) {
+     window.confirm("Use lower case letters?");
+   }
+ 
+ }
+  
